d61dd5f7f079d404d13acdefe81ebad82ba68f0f
aminfranko <aminfranko@gmail.com>
2022-01-13 20:21:45 +0330

    Added base html file
    Added base html file and used extends in website

--- website/urls.py
+++ website/urls.py
@@ -4,5 +4,11 @@ from website.views import *
 urlpatterns = [
     path('', index_view),
     path('about', about_view),
-    path('contact', contact_view)
+    path('contact', contact_view),
+    path('portfolio-details', portfolio_details_view),
+    path('portfolio', portfolio_view),
+    path('pricing', pricing_view),
+    path('services', services_view),
+    path('team', team_view),
+    path('testimonials', testimonials_view),
 ]
--- website/views.py
+++ website/views.py
@@ -7,10 +7,29 @@ from django.http import HttpResponse, JsonResponse
 def index_view(request):
     return render(request, 'website/index.html')
 
+def contact_view(request):
+    return render(request, 'website/contact.html')
+
 def about_view(request):
     return render(request, 'website/about.html')
 
-def contact_view(request):
-    return render(request, 'website/contact.html')
+def portfolio_details_view(request):
+    return render(request, 'website/portfolio-details.html')
+
+def portfolio_view(request):
+    return render(request, 'website/portfolio.html')
+
+def pricing_view(request):
+    return render(request, 'website/pricing.html')
+
+def services_view(request):
+    return render(request, 'website/services.html')
+
+def team_view(request):
+    return render(request, 'website/team.html')
+
+def testimonials_view(request):
+    return render(request, 'website/testimonials.html')
+
 
 
