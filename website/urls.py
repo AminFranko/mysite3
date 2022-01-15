1a9b0e467eb38989bf4d60d7923dafa7b4585191
aminfranko <aminfranko@gmail.com>
2022-01-15 19:40:09 +0330

    Added Contact form and Newsletter Form
    Added Contact form and Newsletter Form in website

--- website/urls.py
+++ website/urls.py
@@ -13,4 +13,5 @@ urlpatterns = [
     path('services', services_view,name='services'),
     path('team', team_view,name='team'),
     path('testimonials', testimonials_view,name='testimonials'),
+    path('newsletter',newsletter_view,name='newsletter')
 ]
--- website/views.py
+++ website/views.py
@@ -1,5 +1,7 @@
 from django.shortcuts import render
-from django.http import HttpResponse, JsonResponse
+from django.http import HttpResponse,JsonResponse,HttpResponseRedirect
+from website.models import Contact
+from website.forms import NameForm,ContactForm,NewsletterForm
 # Create your views here.
 
 
@@ -7,8 +9,29 @@ from django.http import HttpResponse, JsonResponse
 def index_view(request):
     return render(request, 'website/index.html')
 
+# def contact_view(request):
+#     return render(request, 'website/contact.html')
+
+
 def contact_view(request):
-    return render(request, 'website/contact.html')
+    if request.method == 'POST':
+        form = ContactForm(request.POST)
+        if form.is_valid():
+            form.save()
+    form = ContactForm()
+    return render(request,'website/contact.html',{'form':form})
+
+
+def newsletter_view(request):
+    if request.method == 'POST':
+        form = NewsletterForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return HttpResponseRedirect('/')
+    else:
+        return HttpResponseRedirect('/')
+
+
 
 def about_view(request):
     return render(request, 'website/about.html')
